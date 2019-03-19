a94977256dbca8fd566878cc7a36ebb8d94fc488
DieGabelLinksVomTeller <thomas.schell35@web.de>
2019-03-19 12:59:40 +0100

    added empty line handling

--- Sudoku/Io/JaggedByteFieldReader.cs
+++ Sudoku/Io/JaggedByteFieldReader.cs
@@ -1,5 +1,6 @@
 ﻿using Foobar999.Sudoku.Interface;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -9,7 +10,29 @@ namespace Foobar999.Sudoku.Io
 	{
 		public Byte[][] Read(String filePath)
 		{
-			return File.ReadLines(filePath).Select(x => x.Split().Select(Byte.Parse).ToArray()).ToArray();
+			List<Byte[]> field = new List<Byte[]>();
+
+			Int32 rowIndex = 0;
+			foreach (String line in File.ReadLines(filePath))
+			{
+				List<Byte> row = new List<Byte>();
+
+				Int32 columnIndex = 0;
+				foreach (String token in line.Split((Char[])null, StringSplitOptions.RemoveEmptyEntries))
+				{
+					Boolean isValidElement = Byte.TryParse(token, out Byte element);
+					if (!isValidElement)
+					{
+						throw new ArgumentOutOfRangeException(nameof(filePath), $"Invalid element at row {rowIndex}, column {columnIndex}");
+					}
+					row.Add(element);
+					columnIndex += 1;
+				}
+				field.Add(row.ToArray());
+				rowIndex += 1;
+			}
+
+			return field.Where(x => x.Length > 0).ToArray();
 		}
 	}
 }
